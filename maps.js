03e2e3dd3128fd5f37efd69d0aa292710ade0cb5
crisbravo1 <cristhian3bravo@gmail.com>
2015-11-28 19:05:59 +0000

    google maps with marker on initial position

--- maps.js
+++ maps.js
@@ -2,12 +2,24 @@
 function initialize() {
 
 
-    var map = new google.maps.Map();
+    /*Parametros del mapa para la funcion map */
+    var mapOptions = {
+
+      center: new google.maps.LatLng(44.5403, -78.5463),
+      zoom: 8,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+
+    }
 
 
+    var mapCanvas = document.getElementById('map');
 
 
+    var map = new google.maps.Map(mapCanvas);
 
 
 }
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
 
